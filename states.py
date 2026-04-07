23ffce17e4cdc384622c1db6b40b751621a2824d
man970934-create <man970934@gmail.com>
2026-04-07 18:58:58 +0300

    Add FeedbackStates class to states.py

--- states.py
+++ states.py
@@ -3,3 +3,6 @@ from aiogram.fsm.state import State, StatesGroup
 class BroadcastStates(StatesGroup):
     waiting_for_message = State()
     confirm = State()
+
+class FeedbackStates(StatesGroup):
+    waiting_for_text = State()
